7f10ac7a60f7c952dad4ef83a8e4d47fb4043dba
nguyethy002 <minhthy0120032003@gmail.com>
2020-05-16 18:00:57 -0700

    5/16/20: working chart

--- src/api/anime.js
+++ src/api/anime.js
@@ -1,20 +1,31 @@
 import React, {useState, useEffect} from 'react';
 import "../styles/api/Anime.style.sass"
-
+import { Bar } from 'react-chartjs-2';
 const DataFetching = () => {
   const [results, setResults] = useState([]);
+  const [ratings, setRatings] = useState([]);
+  const ratingArray = [];
   const url = "https://kitsu.io/api/edge/anime";
 
   useEffect(()=>{
     fetch(url)
     .then((response)=> response.json())
     .then((data) => {
-      let characters = data.data;
-      console.log(characters)
-      setResults(characters)
+      let anime = data.data;
+      // console.log(anime)
+      // console.log(typeof anime[0].attributes.ratingFrequencies)
+      // anime.forEach((item) =>{
+      //   console.log(item.attributes.ratingFrequencies)
+      // })
+      for(var i = 0; i < anime.length; i++){
+        ratingArray.push(anime[i].attributes.ratingFrequencies)
+      }
+      setResults(anime)
+      setRatings(ratingArray);
     })
     .catch((error) => console.log(error));
   },[])
+  console.log("this is ratings", ratings);
   return (
     <div className = "anime">
         {results.map(result => {      
@@ -26,7 +37,11 @@ const DataFetching = () => {
           }
           return null;
         })}
-
+        {/* <Bar
+          data={
+            labels: 
+          }
+        /> */}
     </div>
   ); 
 };
